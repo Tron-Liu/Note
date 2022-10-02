19110b6b9804507961317bddb1e4e6faf1f8b972
Tron-Liu <Tron-Liu@foxmail.com>
2022-10-02 18:49:57 +0800

    重构代码

--- book/Linux 高性能服务器编程/ch11 定时器/code/test2.cpp
+++ book/Linux 高性能服务器编程/ch11 定时器/code/test2.cpp
@@ -11,95 +11,36 @@
 #ifndef LST_TIMER
 #define LST_TIMER
 
+#include <arpa/inet.h>
+#include <sys/socket.h>
 #include <time.h>
 
 #define BUFFER_SIZE 64
 
 // 前向声明
-class util_timer;
+class UtilTimer;
 
-//
+// 用户数据结构
 struct client_data {
   sockaddr_in address;    // 客户端 socket 地址
   int sockfd;             // socket 文件描述符
   char buf[BUFFER_SIZE];  // 读缓存
-  util_timer* timer;      // 定时器
+  UtilTimer* timer;       // 定时器
 };
 
-class util_timer {
+// 定时器类
+class UtilTimer {
+ public:
+  UtilTimer() : prev(NULL), next(NULL) {}
+
  public:
   time_t expire;  // 任务超时时间，这里使用绝对时间
   void (*cb_func)(client_data*);  // 任务回调函数
+
   // 回调函数处理的客户数据，由定时器的执行者传递给回调函数
   client_data* user_data;
-  util_timer* prev;
-  util_timer* next;
+  UtilTimer* prev;
+  UtilTimer* next;
 };
-
-class sort_timer_lst {
- public:
-  sort_timer_lst() : head(NULL), tail(NULL) {}
-  ~sort_timer_lst() {
-    util_timer* tmp = head;
-
-    while(tmp) {
-      head = tmp->next;
-      delete tmp;
-      tmp = head;
-    }
-  }
-  void add_timer(util_timer* timer) {
-    if (!timer) return;
-    if (!head) {
-      head = tail = timer;
-      return ;
-    }
-    if (timer->expire < head->expire) {
-      timer->next = head;
-      head->prev = timer;
-      head = timer;
-      return;
-    }
-    add_timer(timer, head);
-  }
-
-  void tick() {
-    if (!head) return;
-    printf("timer tick\n");
-    time_t cur = time(NULL);
-    util_timer* tmp = head;
-
- private:
-  void add_timer(util_timer* timer, util_timer* lst_head) {
-    util_timer* prev = lst_head;
-    util_timer* tmp = prev->next;
-
-    while (tmp) {
-      if (timer->expire < tmp->expire) {
-        prev->next = timer;
-        timer->next = tmp;
-        tmp->prev = timer;
-        timer->prev = prev;
-        break;
-      }
-      prev = tmp;
-      tmp = tmp->next;
-    }
-    
-    if (!tmp) {
-      prev->next = timer;
-      timer->prev = prev;
-      timer->next = NULL;
-      tail = timer;
-    }
-  }
-
- private:
-   util_timer* head;
-   util_timer* tail;
-}
-int main(int argc, char* argv[]) {
-  return 0;
-}
 
 #endif
